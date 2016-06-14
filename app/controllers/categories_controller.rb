fc5cef74f8c76e552b32a03193648f9f06275921
drownedout <danielcdonnelly@gmail.com>
2016-06-14 14:02:08 -0400

    Added Category CRUD and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,23 +1,35 @@
 class CategoriesController < ApplicationController
-	before_action :find_category, only: [:edit, :update, :destroy]
+	before_action :find_category, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@categories = Category.all
+	end
 
 	def new
 		@category = Category.new
 	end
 
-	def update
-		@category = Category.new
+	def create
+		@category = Category.new(category_params)
 		if @category.save
-			redirect_to @category
+			redirect_to root_path
 		else
 			render 'new'
 		end
+	end
+
+	def show
 	end
 
 	def edit
 	end
 
 	def update
+		if @category.update(category_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
@@ -26,7 +38,7 @@ class CategoriesController < ApplicationController
 	private
 
 	def category_params
-
+		params.require(:category).permit(:name)
 	end
 
 	def find_category
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	devise_for :users
 	resources :versions, only: [:index]
+	resources :categories
 
 	resources :articles do
 		collection do
