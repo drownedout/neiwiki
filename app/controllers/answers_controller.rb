f4a24dc885039c54d52efd8af78fe2066f4492bb
drownedout <danielcdonnelly@gmail.com>
2016-07-26 21:08:40 -0400

    Changes to answers and forums controller, views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,23 @@
 class AnswersController < ApplicationController
+	def create
+		@forum = Forum.find(params[:forum_id])
+		@answer = @forum.comments.create(params[:answer].permit(:name, :body))
+		@answer.user_id = current_user.id
+		if @answer.save
+			respond_to do |format|
+        		format.html { redirect_to article_path(@article) }
+        		format.js 
+    		end
+		else
+			flash.now = "error"
+		end
+	end
+
+	def destroy
+		@forum = Forum.find(params[:forum_id])
+		@answer = @forum.answers.find(params[:id])
+    	@answer.destroy
+
+    	redirect_to forum_path(@forum)
+	end
 end
--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,2 +1,53 @@
 class ForumsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :find_forum, only: [:show, :edit, :update, :destroy]
+	
+	def index
+		@forums = Forum.all
+	end
+
+	def new
+		@forum = current_user.forums.build 
+	end
+
+	def create
+		@forum = current_user.forums.build(forum_params)
+
+		if @forum.save
+			redirect_to @forum
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @forum.update(forum_params)
+			redirect_to @forum
+		else
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@forum.destroy
+		redirect_to forum_path
+	end
+
+	private
+
+	def find_forum
+		@forum = Forum.find(params[:id])
+	end
+
+	def forum_params
+		params.require(:forum).permit(:title, :content)
+	end
+
 end
