76da8f5dd2da60be1b996a6b9dcd5d199f8cabbe
drownedout <danielcdonnelly@gmail.com>
2016-11-15 16:01:24 -0500

    Added profile pictures to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,10 @@ class User < ActiveRecord::Base
   has_many :answers
   validates_acceptance_of :terms, acceptance: true, message: "You must accept the terms and conditons to use the NEI Wiki"
 
-	def full_name
+	has_attached_file :profile_picture, styles: { medium: "500x500#", small: "100x100#" }, :default_url => ActionController::Base.helpers.asset_path('default-avatar.png')
+  validates_attachment_content_type :profile_picture, :content_type => /\Aimage\/.*\Z/
+
+  def full_name
 		return firstname + " " + lastname
 	end
   
