06b903020b77344738075ad45b46bbf8ae55b1d1
drownedout <danielcdonnelly@gmail.com>
2016-11-17 15:37:18 -0500

    Updated homepage to include forums rather than categories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 	before_action :authenticate_user!, except: [:terms]
 
 	def home
-		@categories = Category.order("created_at DESC").all.limit(6)
+		@forums = Forum.order("created_at DESC").all.limit(6)
 		@articles = Article.order("created_at DESC").all.limit(6)
 	end
 
